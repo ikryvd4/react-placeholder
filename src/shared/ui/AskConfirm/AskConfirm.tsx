94040087949797e1b83a447913f7c5525488a253
schwartz <gtfsolo@gmail.com>
2025-11-06 21:00:14 +0200

    Uppdate components AskConfirm

--- src/shared/ui/AskConfirm/AskConfirm.tsx
+++ src/shared/ui/AskConfirm/AskConfirm.tsx
@@ -1,7 +1,3 @@
-// import { useCounter } from "@app/context/counter/CounterProvider";
-
-// import { onRandom } from "@app/context/counter/action";
-
 import "./AskConfirm.css";
 
 type AskConfirmType = {
@@ -12,17 +8,20 @@ type AskConfirmType = {
 
 export function AskConfirm({ onClose, onConfirm, title }: AskConfirmType) {
   return (
-    <div className="ask-reset">
-      <h2 className="ask-reset__title">{title}</h2>
+    <div className="ask-confirm">
+      <h2 className="ask-confirm__title">{title}</h2>
 
-      <div className="ask-reset__buttons">
+      <div className="ask-confirm__buttons">
         <button
-          className="ask-reset__btn ask-reset__btn--yes"
+          className="ask-confirm__btn ask-confirm__btn--yes"
           onClick={onConfirm}
         >
           Yes
         </button>
-        <button className="ask-reset__btn ask-reset__btn--no" onClick={onClose}>
+        <button
+          className="ask-confirm__btn ask-confirm__btn--no"
+          onClick={onClose}
+        >
           No
         </button>
       </div>
